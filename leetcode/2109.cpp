502cd82315b9884fc4c294111f982fe562aa23e1
nxmai <nxmai.it@gmail.com>
2021-12-20 20:31:02 +0700

    add solution for 2109

---
+++ leetcode/2109.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string res = "";
+        
+        int n = s.size();
+        int m = spaces.size();
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(j < m && i == spaces[j]){
+                res += " ";
+                j++;
+            }
+            res += s[i];
+        }
+        return res;
+    }
+};
